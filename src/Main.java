e8cf3693d8789ba542024d47a427a4772097b07a
Chunky105 <137158062+TomCas105@users.noreply.github.com>
2024-03-05 17:03:57 +0100

    Pouzitie lambdy

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> numbers = new ArrayList();
+        for (int i = 0; i < 10; i++) {
+            numbers.add(i + 1);
+        }
+        numbers.forEach((number) -> {
+            System.out.println(number);
+        });
     }
 }
